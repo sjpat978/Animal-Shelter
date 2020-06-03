21420e131f0fd24223ed3c51572ee32fee6ea5a0
Kanishka0710 <kanishkaroy0710@gmail.com>
2020-06-03 12:41:14 -0700

    asd

--- assets/js/database-script.js
+++ assets/js/database-script.js
@@ -1,42 +1,42 @@
+let sortDirection = false;
 
 let animalData = [{
         animalType: 'Dog',
-        name = 'Sparky',
-        age = 4,
-        ID = 19234,
-        breed = 'saint bernard',
-        medicationsNeed = 'none',
-        taken = 'not applicable'
-    },
-    {
-        animalType: 'Hamster',
-        name = 'Sparky',
-        age = 4,
-        ID = 19236,
-        breed = 'saint bernard',
-        medicationsNeed = 'none',
-        taken = 'not applicable'
-    },
-    {
-        animalType: 'Rabbit',
-        name = 'Sparky',
-        age = 4,
-        ID = 19237,
-        breed = 'saint bernard',
-        medicationsNeed = 'none',
-        taken = 'not applicable'
+        name: 'Sparky',
+        age: 4,
+        ID: 19234,
+        breed: 'saint bernard',
+        medicationsNeed: 'none',
+        taken: 'not applicable'
     },
     {
         animalType: 'Dog',
-        name = 'Sparky',
-        age = 4,
-        ID = 19238,
-        breed = 'saint bernard',
-        medicationsNeed = 'none',
-        taken = 'not applicable'
+        name: 'Sparky',
+        age: 4,
+        ID: 19234,
+        breed: 'saint bernard',
+        medicationsNeed: 'none',
+        taken: 'not applicable'
+    },
+    {
+        animalType: 'Dog',
+        name: 'Sparky',
+        age: 4,
+        ID: 19234,
+        breed: 'saint bernard',
+        medicationsNeed: 'none',
+        taken: 'not applicable'
+    },
+    {
+        animalType: 'Dog',
+        name: 'Sparky',
+        age: 4,
+        ID: 19234,
+        breed: 'saint bernard',
+        medicationsNeed: 'none',
+        taken: 'not applicable'
     },
 ]
-
 class Animal {
     constructor(type, name, age, ID, breed, medNeed) {
         this.animalType = type;
@@ -47,8 +47,6 @@ class Animal {
         this.medicationsNeed = medNeed;
     }
 }
-
-
 window.onload = () => {
     loadTableData(animalData);
 }
@@ -58,27 +56,37 @@ function loadTableData(data) {
     let dataHTML = '';
     for (let animal of data) {
         dataHTML += `<tr><td>${animal.animalType}</td>
-                    <td>${animal.name}</td> 
-                    <td>${animal.age}</td>
-                    <td>${animal.ID}</td>
-                    <td>${animal.breed}</td>
-                    <td>${animal.medicationsNeed}</td>
-                    <td>${animal.taken}</td>
-                    </tr>`
+                <td>${animal.name}</td> 
+                <td>${animal.age}</td>
+                <td>${animal.ID}</td>
+                <td>${animal.breed}</td>
+                <td>${animal.medicationsNeed}</td>
+                <td>${animal.taken}</td>
+                </tr>`
     }
 
-    console.log(dataHTML);
-    tableBody.innerHTML = dataHTML
+    //console.log(dataHTML);
 
+    tableBody.innerHTML = dataHTML;
 }
 
 document.getElementById('database-addbtn').addEventListener('click', () => {
     console.log('add button clicked')
-    const tableFoot = document.getElementById('database-data')
-    const addBtns = [document.getElementById('addAnimalbtn'), document.getElementById('database-cancelbtn')]
-    tableFoot.style.display = ";"
-    for (let i = 0; i < addBtns.length(); i++) {
+    const tableFoot = document.getElementById('database-footer')
+    const addBtns = [document.getElementById('addAnimalbtn'), document.getElementById(
+        'database-cancelbtn')]
+    tableFoot.style.display = "";
+    for (let i = 0; i < addBtns.length; i++) {
         addBtns[i].style.display = "inline-block";
+    }
+});
+document.getElementById('database-cancelbtn').addEventListener('click', () => {
+    const tableFoot = document.getElementById('database-footer')
+    const addBtns = [document.getElementById('addAnimalbtn'), document.getElementById(
+        'database-cancelbtn')]
+    tableFoot.style.display = "none";
+    for (let i = 0; i < addBtns.length; i++) {
+        addBtns[i].style.display = "none";
     }
 });
 
@@ -90,24 +98,24 @@ document.getElementById('addAnimalbtn').addEventListener('click', () => {
     let animalID = document.getElementById('IDnumber-form').value
     let animalBreed = document.getElementById('breed-form').value
     let meds = document.getElementById('medications-form').value
-
-    const newAnimal = Animal(animalType, animalName, animalAge, animalID, animalBreed, meds)
+    const newAnimal = new Animal(animalType, animalName, animalAge, animalID, animalBreed, meds)
     loadDataform(newAnimal)
-
+    const tableFoot = document.getElementById('database-footer')
+    const addBtns = [document.getElementById('addAnimalbtn'), document.getElementById(
+        'database-cancelbtn')]
+    tableFoot.style.display = "none";
+    for (let i = 0; i < addBtns.length; i++) {
+        addBtns[i].style.display = "none";
+    }
 });
 
 function loadDataform(Animal) {
     const tableBody = document.getElementById('database-data');
-    let dataHTML = '';
-    for (const property in Animal) {
-        dataHTML += `<tr><td>${animal.animalType}</td>
-                    <td>${animal.name}</td> 
-                    <td>${animal.age}</td>
-                    <td>${animal.ID}</td>
-                    <td>${animal.breed}</td>
-                    <td>${animal.medicationsNeed}</td>
-                    </tr>`
+    let dataHTML = '<tr>';
+    for (let property in Animal) {
+        dataHTML += `<td>${property}</td>`
     }
+    dataHTML += "</tr>"
 
     console.log(dataHTML)
     tableBody.innerHTML = dataHTML
