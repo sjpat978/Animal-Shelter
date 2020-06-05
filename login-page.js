b262cdde4065152d587600c7f857fa5a4aceeab6
Sanjana <sanjanapendharkar01@gmail.com>
2020-06-05 15:49:05 -0400

    login

--- login-page.js
+++ login-page.js
@@ -1,28 +1,24 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+//const loginErrorMsg = document.getElementById("login-error-msg");
 
 // When the login button is clicked, the following code is executed
-loginButton.addEventListener("click", (e) => {
+/*loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
     e.preventDefault();
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    valid();
-    /*if (username === "user" && password === "web_dev") {
+    if (username === "user" && password === "web_dev") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         location.reload();
     } else {
         // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
-    }*/
-});
-
-function valid() {
+        alert("Access Denied.");
+    } 
     var username = document.login.username.value;
-    var passwrod = document.login.password.value;
+    var password = document.login.password.value;
     var valid = false;
     var usernameArray = ["Bob", "Joe"];
     var passwordArray = ["12345", "54321"];
@@ -33,9 +29,25 @@ function valid() {
         }
     }
     if (valid == true) {
-        console.log("Login successful");
+        alert("Login successful");
     }
     else {
-        console.log("Access Denied");
+        alert("Access Denied");
+    }
+});*/
+
+function valid() {
+    var uname = document.forms["login-form"]["uname"].value;
+    var pass = document.forms["login-form"]["pass"].value;
+    if (uname == "animalluver") {
+        if (pass == "123") {
+            alert("login successful");
+        }
+        else {
+            alert("incorrect");
+        }
+    }
+    else {
+        alert("incorrect");
     }
 }
