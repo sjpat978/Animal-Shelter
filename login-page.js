9a34168566d5c8da206811f1a6242a2f241fbb6b
Sanjana <sanjanapendharkar01@gmail.com>
2020-06-05 10:37:18 -0400

    login

--- login-page.js
+++ login-page.js
@@ -9,13 +9,33 @@ loginButton.addEventListener("click", (e) => {
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
+    valid();
+    /*if (username === "user" && password === "web_dev") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         location.reload();
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
+    }*/
+});
+
+function valid() {
+    var username = document.login.username.value;
+    var passwrod = document.login.password.value;
+    var valid = false;
+    var usernameArray = ["Bob", "Joe"];
+    var passwordArray = ["12345", "54321"];
+    for (var i = 0; i < usernameArray.length; i++) {
+        if ((username == usernameArray[i]) && (password = passwordArray[i])) {
+            valid = true;
+            break;
+        }
     }
-})
+    if (valid == true) {
+        console.log("Login successful");
+    }
+    else {
+        console.log("Access Denied");
+    }
+}
